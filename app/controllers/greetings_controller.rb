fee55b6b97c6d0c40c86f0051f81378608577aaa
Waweru <ngashalex8@gmail.com>
2023-10-04 13:15:30 +0300

    linters

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,7 +1,6 @@
 class GreetingsController < ApplicationController
-    def random_greeting
-      @random_greeting = Message.order("RANDOM()").first
-      render json: @random_greeting
-    end
+  def random_greeting
+    @random_greeting = Message.order('RANDOM()').first
+    render json: @random_greeting
   end
-  
+end
