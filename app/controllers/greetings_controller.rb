c8269faa6c70cba6592d12569e5f7fa939595a7a
Waweru <ngashalex8@gmail.com>
2023-10-04 12:43:14 +0300

    Setup complete

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,7 @@
+class GreetingsController < ApplicationController
+    def random_greeting
+      @random_greeting = Message.order("RANDOM()").first
+      render json: @random_greeting
+    end
+  end
+  
